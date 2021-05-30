3c01535654c73d508faea368c3a61625f45792ce
zeamaria <lingardzea@gmail.com>
2021-05-30 15:11:02 -0700

    Add use visual mode to appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,31 +5,37 @@ import Show from "components/Appointment/Show";
 import Form from "components/Appointment/Form";
 import Status from "components/Appointment/Status";
 import "components/Appointment/styles.scss";
+import {useVisualMode} from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
 
-  function save(name, interviewer) {
-    const interview = {
-      student: name,
-      interviewer,
-    }};
-    console.log(props.interview)
-    console.log(props.time)
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
-      <Header time={props.time}></Header>
-      {props.interview ? (
-        <Show
-          student={props.interview.student}
-          interviewer={props.interview.interviewer}
-        />
-      ) : (
-        <Empty />
-      )}
-
+      <Header time={props.time}/>
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+    {mode === SHOW && (
+      <Show 
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      />
+    )}
+    {mode === CREATE && (
       <Form 
-      interviewers={props.interviewers} 
-      save={save}/>
+        name={props.name}
+        interviewers={[]}
+        value={props.value}
+        onSave={() => console.log("onSave")}
+        onCancel={() => back()}
+      />
+    )}
     </article>
-  );
-}
+    )
+};
