e2f964e3290a426a130d071aea4bbe68f2456131
zeamaria <lingardzea@gmail.com>
2021-05-30 15:30:15 -0700

    Add cancel interview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
@@ -10,12 +10,28 @@ import {useVisualMode} from "hooks/useVisualMode";
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const STATUS = "STATUS";
 
 export default function Appointment(props) {
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  const [message, setMessage] = useState("");
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+
+    setMessage("Saving...");
+    transition(STATUS);
+
+    props.bookInterview(props.id, interview)
+    .then(() => transition(SHOW));
+  }
 
   return (
     <article className="appointment">
@@ -30,12 +46,18 @@ export default function Appointment(props) {
     {mode === CREATE && (
       <Form 
         name={props.name}
-        interviewers={[]}
+        interviewers={props.interviewers}
         value={props.value}
-        onSave={() => console.log("onSave")}
+        onSave={save}
         onCancel={() => back()}
       />
     )}
+    {mode === STATUS && (
+      <Status
+        message={message}
+      />
+    )
+    }
     </article>
     )
 };
