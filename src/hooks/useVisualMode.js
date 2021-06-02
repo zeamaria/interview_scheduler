7e749d2dc17ec1324008d116234d9e03d3dfedfd
zeamaria <lingardzea@gmail.com>
2021-06-02 14:13:56 -0700

    Fix Stale State. Return to Create Form

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,11 +4,12 @@ export function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  
+  console.log(history)
   function transition(newMode, replace = false) {
+    console.log("transition", newMode, replace)
     setMode(newMode) 
     if(replace){
-      setHistory([...history.slice(0, -1), newMode]);
+      setHistory((prev) => [...prev.slice(0, -1), newMode]);
     }
     else {
     setHistory([...history, newMode])
@@ -16,6 +17,7 @@ export function useVisualMode(initial) {
   }
 
   function back() {
+    console.log("back", history)
     if (history.length === 1) {
       return;
     } else {
